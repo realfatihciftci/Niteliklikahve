f3ba982d550bc102b55c339469fc638c8691ed7c
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-04-27 16:59:04 +0300

    CoffeeBean modeli eklendi

---
+++ NitelikliKahve/Models/CoffeeBean.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NitelikliKahve.Models;
+
+public class CoffeeBean
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Marka alanı zorunludur.")]
+    [MaxLength(25, ErrorMessage = "Marka en fazla 25 karakter olabilir.")]
+    public string Brand { get; set; }
+    
+    [Required(ErrorMessage = "Yöre alanı zorunludur.")]
+    [MaxLength(25, ErrorMessage = "Yöre en fazla 25 karakter olabilir.")]
+    public string Origin { get; set; }
+    
+    [MaxLength(25, ErrorMessage = "Kavrum derecesi en fazla 25 karakter olabilir.")]
+    public string RoastLevel { get; set; }
+}
