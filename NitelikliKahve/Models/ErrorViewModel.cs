5d3ec455b49d1cad79b9551fb2a3c712e7be7d0c
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-04-27 16:44:55 +0300

    v0 başlangıç

---
+++ NitelikliKahve/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NitelikliKahve.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
